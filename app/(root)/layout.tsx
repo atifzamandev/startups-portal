510729d8734dcd4d7322121f29880fca4627daea
Atif Zaman <atifzaman88010@gmail.com>
2024-12-15 11:54:27 +0100

    Project and github authentication setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react'
+import NavBar from '../components/NavBar'
+
+interface LayoutProps {
+  children: ReactNode
+}
+const layout = ({ children }: Readonly<LayoutProps>) => {
+  return (
+    <main className='font-sans'>
+      <NavBar />
+      {children}
+    </main>
+  )
+}
+
+export default layout
