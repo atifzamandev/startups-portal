6bebe606400aae3a24a3dd5c4318be6c9bf34ef1
Atif Zaman <atifzaman88010@gmail.com>
2024-12-31 15:08:12 +0100

    Install and setup Sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '../../components/SearchForm'
+import StartupCard from '@/components/StartupCard'
 
 interface HomeProps {
   searchParams: Promise<{ query: string }>
@@ -6,6 +7,19 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   const query = (await searchParams).query
+  const posts = [
+    {
+      _createdAt: 'Yesterday',
+      views: 78,
+      author: { _id: 1, name: 'Atif' },
+      _id: 1,
+      description: 'This is startup description',
+      image:
+        'https://images.pexels.com/photos/8566531/pexels-photo-8566531.jpeg',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ]
 
   return (
     <>
@@ -17,6 +31,22 @@ export default async function Home({ searchParams }: HomeProps) {
           Submit Ideal, Vote on Pictches and Get Noticed for Startup Finance.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search results for ${query}` : 'All startups'}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts.length > 0 ? (
+            posts.map((posts: StartupCardType) => (
+              <StartupCard key={posts?._id} posts={posts} />
+            ))
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string) {
+  let givenDate: Date
+
+  if (date.toLowerCase() === 'yesterday') {
+    givenDate = new Date()
+    givenDate.setDate(givenDate.getDate() - 1)
+  } else {
+    givenDate = new Date(date)
+  }
+
+  if (isNaN(givenDate.getTime())) {
+    throw new Error(`Invalid date: ${date}`)
+  }
+
+  return givenDate.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  })
 }
